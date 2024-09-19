72a7e18970dc7c6f1f223cca13b7f8ec32f6e119
Stephanie Micah Cahutay <stephanie.m.cahutay@accenture.com>
2024-09-19 10:54:36 +0800

    Data Binding & Types #3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,29 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
-  title = 'angular-intro';
+  title: string = '1.Introduction';
+  myBtn: string = 'My Button';
+  counter: number = 0;
+
+  //attribute binding
+  isDisabled: boolean = false;
+  angularImage: string = '../assets/angular16.jpg';
+
+  //property binding
+  bgColor: string = 'red';
+  titleColor: string = 'white';
+  description: string = 'font-size: 30px; color: lightblue';
+
+  //event binding
+  incrementCounter() {
+    this.counter++;
+  }
+
+  //class binding
+  redText: boolean = true;
+
 }
