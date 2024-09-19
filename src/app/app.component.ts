dab841f0346b6dae4f4263dff94bc035b1f8d03c
Stephanie Micah Cahutay <stephanie.m.cahutay@accenture.com>
2024-09-19 13:42:59 +0800

    Use built-in and custom pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,23 +2,20 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { AppendPipe } from './pipes/append.pipe';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, CommonModule],
+  imports: [RouterOutlet, FormsModule, CommonModule, AppendPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 
 export class AppComponent {
-  title: string = 'Structural Directives';
-  isLoggedIn: boolean = false;
-  username: string = 'Stephanie Micah';
+  title: string = 'Pipes';
 
-  //loops
-  names: string[] = ['steph', 'micah', 'ali'];
+  today: number = Date.now();
 
-  //switch
-  grade: string = 'B';
+  currency: number = 1.3244;
 }
