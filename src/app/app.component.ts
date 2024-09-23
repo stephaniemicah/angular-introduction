8d4472175502881dbeb6a1c034f1b29786236382
Stephanie Micah Cahutay <stephanie.m.cahutay@accenture.com>
2024-09-23 10:39:49 +0800

    Template-driven Forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,69 +1,27 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-import { MessagesService } from './services/messages.service';
-import { HttpClientModule } from '@angular/common/http';
-import { Post } from './interfaces/posts.interface';
-import { LatestPrices, OrderBook, Student } from './interfaces/data.interface';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, CommonModule, HttpClientModule],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  providers: [MessagesService]
 })
 
-export class AppComponent implements OnInit {
-  messages: string[] = [];
-  posts: Post[] = [];
-  languages: Array<string> = [];
-  student: Student | null = null;
+export class AppComponent  {
+  user: { name: string; email: string } = {
+    name: '',
+    email: '',
+  };
 
-  constructor(private messagesService: MessagesService) {
-    this.messages = messagesService.getMessages();
+  submitForm(form: NgForm) {
+    if (form.valid) {
+      console.log(form.value, this.user);
+    }
   }
-
-  ngOnInit() {
-  //   this.messagesService.getPosts().subscribe(
-  //     (response) => {
-  //       this.posts = response;
-  //     },
-  //   (error) => {
-  //     console.error(error);
-  //   }
-  //  );
-
-      this.messagesService.getPosts().subscribe({
-        next: (response: Post[]) => {this.posts = response;},
-        error: (error) => {console.error(error);}
-      });
-
-      this.messagesService.getLanguages().subscribe({
-        next: (response: Array<string>) => {this.languages = response;},
-        error: (error) => {console.error(error);}
-      });
-
-      this.messagesService.getStudent().subscribe({
-        next: (response: Student) => {this.student = response;},
-        error: (error) => {console.error(error);}
-      });
-
-      this.messagesService.getBookOrders().subscribe({
-        next: (bookOrders: OrderBook) => {
-          console.log(bookOrders);
-        }
-      });
-
-      this.messagesService.getLatestPrices().subscribe({
-        next: (latestPrices: LatestPrices) => {
-          console.log(latestPrices);
-        }
-      });
-  }
-
-  title: string = 'Dependency Injection & Services';
 
 }
